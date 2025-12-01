387d5ab51534069ca6260a27a24ef3c78cef732e
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-11-30 23:12:23 -0500

    Update material.hpp

--- Graphics/Geometry/material.hpp
+++ Graphics/Geometry/material.hpp
@@ -1,27 +1,32 @@
 #pragma once
 
 #include <Math/math.hpp>
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <assimp/anim.h>
+#include <assimp/version.h>
 
 using GLTextureID = int;
 enum TextureType {
-    TEXTURE_TYPE_UNKNOWN = 0,
-    TEXTURE_TYPE_DIFFUSE,
-    TEXTURE_TYPE_SPECULAR,
-    TEXTURE_TYPE_AMBIENT,
-    TEXTURE_TYPE_EMISSIVE,
-    TEXTURE_TYPE_HEIGHT,
-    TEXTURE_TYPE_NORMALS,
-    TEXTURE_TYPE_SHININESS,
-    TEXTURE_TYPE_OPACITY,
-    TEXTURE_TYPE_DISPLACEMENT,
-    TEXTURE_TYPE_LIGHT_MAP,
-    TEXTURE_TYPE_REFLECTION,
+    TEXTURE_TYPE_NONE = aiTextureType_NONE,
+    TEXTURE_TYPE_DIFFUSE = aiTextureType_DIFFUSE,
+    TEXTURE_TYPE_SPECULAR = aiTextureType_SPECULAR,
+    TEXTURE_TYPE_AMBIENT = aiTextureType_AMBIENT,
+    TEXTURE_TYPE_EMISSIVE = aiTextureType_EMISSIVE,
+    TEXTURE_TYPE_HEIGHT = aiTextureType_HEIGHT,
+    TEXTURE_TYPE_NORMALS = aiTextureType_NORMALS,
+    TEXTURE_TYPE_SHININESS = aiTextureType_SHININESS,
+    TEXTURE_TYPE_OPACITY = aiTextureType_OPACITY,
+    TEXTURE_TYPE_DISPLACEMENT = aiTextureType_DISPLACEMENT,
+    TEXTURE_TYPE_LIGHTMAP = aiTextureType_LIGHTMAP,
+    TEXTURE_TYPE_REFLECTION = aiTextureType_REFLECTION,
 
     TEXTURE_COUNT
 };
 
 static const char* texture_to_string[TEXTURE_COUNT] = {
-    stringify(TEXTURE_TYPE_UNKNOWN),
+    stringify(TEXTURE_TYPE_NONE),
     stringify(TEXTURE_TYPE_DIFFUSE),
     stringify(TEXTURE_TYPE_SPECULAR),
     stringify(TEXTURE_TYPE_AMBIENT),
@@ -31,7 +36,7 @@ static const char* texture_to_string[TEXTURE_COUNT] = {
     stringify(TEXTURE_TYPE_SHININESS),
     stringify(TEXTURE_TYPE_OPACITY),
     stringify(TEXTURE_TYPE_DISPLACEMENT),
-    stringify(TEXTURE_TYPE_LIGHT_MAP),
+    stringify(TEXTURE_TYPE_LIGHTMAP),
     stringify(TEXTURE_TYPE_REFLECTION)
 };
 
