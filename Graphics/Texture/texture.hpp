caff2b3ef747786c8b1c803edb4e3c6436b402e8
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-12-01 14:26:33 -0500

    texture impl

--- Graphics/Texture/texture.hpp
+++ Graphics/Texture/texture.hpp
@@ -1,7 +1,12 @@
-struct Texture {
-    unsigned int id;
-    unsigned int width;
-    unsigned int height;
+#pragma once
 
+struct Texture {
+    unsigned int id = 0;
+    unsigned int width = 0;
+    unsigned int height = 0;
+
+
+    Texture() = default;
     Texture(char* path);
+    Texture(const u8* data, int width, int height, int nrChannels);
 };
