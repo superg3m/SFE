33cccefc887a8b2f50d3dc55c694bacdc79a6a3c
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-12-12 19:40:08 -0500

    naming

---
+++ Renderer/sfe_renderer.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Camera/sfe_camera.hpp"
+#include "Entity/sfe_entity.hpp"
+#include "Geometry/sfe_geometry.hpp"
+#include "Geometry/sfe_material.hpp"
+#include "ShaderBase/sfe_shader_base.hpp"
+#include "sfe_renderer_state.hpp"
+#include "sfe_gl_check.hpp"
